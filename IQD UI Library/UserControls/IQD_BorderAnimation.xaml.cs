f39b68421a8b9a8ec688ec2475898cf66b4c48ea
Abo_Sanad <167923142+AbuTharHatem95@users.noreply.github.com>
2025-03-14 00:23:30 +0300

    Add New User Control (BorderAnmintion)

---
+++ IQD UI Library/UserControls/IQD_BorderAnimation.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace IQD_UI_Library.UserControls
+{
+    /// <summary>
+    /// Interaction logic for IQDBorderAnimation.xaml
+    /// </summary>
+    public partial class IQDBorderAnimation : UserControl
+    {
+        public IQDBorderAnimation()
+        {
+            InitializeComponent();
+        }
+
+
+        // تعريف الخصائص القابلة للتخصيص
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(IQDBorderAnimation), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(Brush), typeof(IQDBorderAnimation), new PropertyMetadata(Brushes.White));
+
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register("BackgroundColor", typeof(Brush), typeof(IQDBorderAnimation), new PropertyMetadata(Brushes.Transparent));
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register("IconSource", typeof(ImageSource), typeof(IQDBorderAnimation), new PropertyMetadata(null));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Brush TextColor
+        {
+            get { return (Brush)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        public Brush BackgroundColor
+        {
+            get { return (Brush)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        public ImageSource IconSource
+        {
+            get { return (ImageSource)GetValue(IconSourceProperty); }
+            set { SetValue(IconSourceProperty, value); }
+        }
+
+    }
+}
