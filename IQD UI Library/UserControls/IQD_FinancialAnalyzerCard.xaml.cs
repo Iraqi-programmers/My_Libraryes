d7c87a35f47bc80a981002a326f4c1b1c6cc958b
Mortaza.n.m <mortazanm92@gmail.com>
2025-03-15 03:10:22 +0300

    Updated IQD_FinancialAnalyzerCard

--- IQD UI Library/UserControls/IQD_FinancialAnalyzerCard.xaml.cs
+++ IQD UI Library/UserControls/IQD_FinancialAnalyzerCard.xaml.cs
@@ -15,11 +15,46 @@ using System.Windows.Shapes;
 
 namespace IQD_UI_Library.UserControls
 {
-    /// <summary>
-    /// Interaction logic for IQD_FinancialAnalyzerCard.xaml
-    /// </summary>
     public partial class IQD_FinancialAnalyzerCard : UserControl
     {
+        public static readonly DependencyProperty StartPointProperty =
+            DependencyProperty.Register("StartPoint", typeof(Point), typeof(IQD_FinancialAnalyzerCard), new PropertyMetadata(new Point(0, 0)));
+
+        public static readonly DependencyProperty EndPointProperty =
+            DependencyProperty.Register("EndPoint", typeof(Point), typeof(IQD_FinancialAnalyzerCard), new PropertyMetadata(new Point(1, 1)));
+
+        public static readonly DependencyProperty GradientColor1Property = 
+            DependencyProperty.Register("GradientColor1", typeof(Color), typeof(IQD_FinancialAnalyzerCard),
+            new PropertyMetadata(Colors.LightBlue)); 
+
+        public static readonly DependencyProperty GradientColor2Property =
+            DependencyProperty.Register("GradientColor2", typeof(Color), typeof(IQD_FinancialAnalyzerCard),
+            new PropertyMetadata(Colors.DodgerBlue)); 
+
+        public Point StartPoint
+        {
+            get { return (Point)GetValue(StartPointProperty); }
+            set { SetValue(StartPointProperty, value); }
+        }
+
+        public Point EndPoint
+        {
+            get { return (Point)GetValue(EndPointProperty); }
+            set { SetValue(EndPointProperty, value); }
+        }
+
+        public Color GradientColor1
+        {
+            get { return (Color)GetValue(GradientColor1Property); }
+            set { SetValue(GradientColor1Property, value); }
+        }
+
+        public Color GradientColor2
+        {
+            get { return (Color)GetValue(GradientColor2Property); }
+            set { SetValue(GradientColor2Property, value); }
+        }
+
         public IQD_FinancialAnalyzerCard()
         {
             InitializeComponent();
