6d50c4d8fcd812b83b566d2fc4b4249ab65b1c1b
Abo_Sanad <167923142+AbuTharHatem95@users.noreply.github.com>
2025-04-04 22:50:46 +0300

    Adding IQD_UC_TextBox_PassTextBox to Library User Control (#4)
    Co-authored-by: mohammed Abbas <108941553+mohammedcoder23@users.noreply.github.com>

---
+++ IQD UI Library/UserControls/IQD_UC_TextBox_PassTextBox.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IQD_UI_Library.UserControls
+{
+    /// <summary>
+    /// Interaction logic for IQD_UC_TextBox_PassTextBox.xaml
+    /// </summary>
+    public partial class IQD_UC_TextBox_PassTextBox : UserControl
+    {
+        public IQD_UC_TextBox_PassTextBox()
+        {
+            InitializeComponent();
+        }
+        public string PlaceHolder
+        {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(IQD_UC_TextBox_PassTextBox));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(IQD_UC_TextBox_PassTextBox));
+
+
+
+        public bool IsPassword
+        {
+            get { return (bool)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsPassword.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordProperty =
+            DependencyProperty.Register("IsPassword", typeof(bool), typeof(IQD_UC_TextBox_PassTextBox));
+
+        private void passbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = PasswordContent.Password;
+        }
+    }
+}
