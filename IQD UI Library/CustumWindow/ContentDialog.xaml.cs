2dad4fcb762bff137f1c26a1fab68651ae395179
mohammed Abbas <108941553+mohammedcoder23@users.noreply.github.com>
2025-04-07 14:26:58 +0300

    Adding Custum Window

---
+++ IQD UI Library/CustumWindow/ContentDialog.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace IQD_UI_Library.CustumWindow
+{
+    /// <summary>
+    /// Interaction logic for ContentDialog.xaml
+    /// </summary>
+    public partial class ContentDialog : Window
+    {
+        public ContentDialog()
+        {
+            InitializeComponent();
+        }
+        public UserControl Content
+        {
+            get;
+            set;
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.LeftButton == MouseButtonState.Pressed)
+        //    {
+        //        brdrOk.Opacity = 0.7;
+        //        DragMove();
+        //    }
+
+        //    if (e.LeftButton == MouseButtonState.Released)
+        //    {
+        //        brdrOk.Opacity = 1;
+        //    }
+
+        //}
+
+        private void ContentFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (this.Content != null)
+            {
+                ContentFrame.Content = this.Content;
+            }
+            else
+            {
+                ContentFrame.Content = null;
+
+            }
+           
+        }
+
+    }
+}
